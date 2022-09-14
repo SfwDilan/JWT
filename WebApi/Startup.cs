0ef64f8ab6206265cfc4beef25272f9cdc088dac
Dilan Budak <dilanbudak04@gmail.com>
2022-09-14 18:00:14 +0300

    .

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -28,6 +28,8 @@ namespace WebApi
         {
             services.AddControllers();
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();  //jwt için
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -44,6 +46,7 @@ namespace WebApi
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+            ServiceTool.Create(services);    //jwt için
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
